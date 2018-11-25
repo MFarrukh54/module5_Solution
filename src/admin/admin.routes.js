30044c17a8135ef75c6b4d823c050f0dda56aedf
MFarrukh54 <farrukh_54@hotmail.com>
2018-11-25 19:04:45 +0500

    Update some route

--- src/admin/admin.routes.js
+++ src/admin/admin.routes.js
@@ -10,19 +10,15 @@ function config($stateProvider, $httpProvider) {
   $stateProvider
 
     // Contains base state that all admin states inherit
-    .state('admin', {
-      url: '/admin',
-      abstract: true,
-      templateUrl: 'src/admin/signUp/signupTemplate.html'
-    })
     // Contains state that all authenticated states inherit
-    .state('admin.signup', {
+    .state('signup', {
       url: '/',
       templateUrl: 'src/admin/signUp/signupTemplate.html',
       controller: 'RegistrationController',
       controllerAs: 'reg'
-    })
-    .state('admin.Info', {
+    });
+    $stateProvider
+    .state('Info', {
       url: '/',
       templateUrl: 'src/admin/registerInfo/registerInfoTemplate.html',
       controller: 'RegistrationInfoController',
@@ -31,32 +27,8 @@ function config($stateProvider, $httpProvider) {
       // Allows us to pass via $state.go(path, params)
       
     })
-    .state('admin.auth.category', {
-      url: '/category/{categoryId}',
-      templateUrl: 'src/admin/category/category-items.html',
-      controller: 'CategoryItemsController',
-      controllerAs: 'categoryItemsCtrl',
-      resolve: {
-        category: ['$stateParams', 'MenuService', function ($stateParams, MenuService) {
-          return MenuService.getCategory($stateParams.categoryId);
-        }],
-        menuItems: ['$stateParams', 'MenuService', function ($stateParams, MenuService) {
-          return MenuService.getMenuItems($stateParams.categoryId);
-        }]
-      }
-    })
-
-    .state('admin.auth.category.menuitem', {
-      url: "/menuitem/{menuItemId}",
-      templateUrl: "src/admin/menu-item/menu-item-edit.html",
-      controller: 'MenuItemEditController',
-      controllerAs: 'menuItemEditCtrl',
-      resolve: {
-        menuItem: ['$stateParams', 'MenuService', function ($stateParams, MenuService) {
-          return MenuService.getMenuItem($stateParams.menuItemId);
-        }]
-      }
-    });;
+    
+    ;
 }
 
 })();
